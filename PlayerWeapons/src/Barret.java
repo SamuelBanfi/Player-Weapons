c438737ab6eba96210feeaf3b3e2459365a47e2e
lorenzo.spadea <lorenzo.spadea@samtrevano.ch>
2021-10-12 18:49:59 +0200

    modi

--- PlayerWeapons/src/Barret.java
+++ PlayerWeapons/src/Barret.java
@@ -5,24 +5,35 @@
  * @version 09.10.2021
  */
 public class Barret extends SniperRifle {
-    
+
+    /**
+     *
+     */
     public Barret() {
-        
         super(10);
     }
 
+    /**
+     *
+     */
     @Override
     protected void makeShootNoise() {
-        
+        System.out.println("STUM");
     }
 
+    /**
+     *
+     */
     @Override
     protected void makeOutOfAmmoNoise() {
-        
+        System.out.println("CHOCK CHOK");
     }
 
+    /**
+     *
+     */
     @Override
     protected void makeReloadNoise() {
-        
+        System.out.println("CLICK SCHLACK CLOCK");
     }
 }
