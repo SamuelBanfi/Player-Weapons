7df7f57042c649132eba66a3703ade2ba0978c02
lorenzo.spadea <lorenzo.spadea@samtrevano.ch>
2021-10-12 21:14:11 +0200

    COMMENTS

--- PlayerWeapons/src/SniperRifle.java
+++ PlayerWeapons/src/SniperRifle.java
@@ -1,5 +1,5 @@
 /**
- * Questa classe rappresenta un giocatore.
+ * Questa classe rappresenta un SniperRifle.
  * @author Samuel Banfi
  * @author Lorenzo Spadea
  * @version 09.10.2021
@@ -7,10 +7,9 @@
 public abstract class SniperRifle extends Weapon {
     
     /**
-     *
+     * Istanzia un oggetto di tipo SniperRifle.
      */
     public SniperRifle(int ammoCapacity) {
-        
         super(ammoCapacity);
     }
 }
